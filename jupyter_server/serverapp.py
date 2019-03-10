217e1b7136f181d96a76923f786835f4ea22afce
Ciaran Langton <email@ciaranlangton.com>
2019-03-10 21:51:29 +0000

    Fix incorrect MIME type for JavaScript

--- jupyter_server/serverapp.py
+++ jupyter_server/serverapp.py
@@ -1484,10 +1484,12 @@ class ServerApp(JupyterApp):
 
     def init_mime_overrides(self):
         # On some Windows machines, an application has registered an incorrect
-        # mimetype for CSS in the registry. Tornado uses this when serving
-        # .css files, causing browsers to reject the stylesheet. We know the
-        # mimetype always needs to be text/css, so we override it here.
+        # mimetype for CSS and JavaScript in the registry.
+        # Tornado uses this when serving .css files, causing browsers to reject the stylesheet.
+        # We know the mimetype always needs to be text/css for css
+        # and application/javascript for JS, so we override it here.
         mimetypes.add_type('text/css', '.css')
+        mimetypes.add_type('application/javascript', '.js')
 
     def shutdown_no_activity(self):
         """Shutdown server on timeout when there are no kernels or terminals."""
