a73a11d66893bc6ec6a907bfed3214720b89da6f
Zsailer <zachsailer@gmail.com>
2019-05-18 17:07:25 -0600

    add server extension submodule to jupyter_server

---
+++ jupyter_server/extension/application.py
@@ -0,0 +1,159 @@
+import sys
+
+from traitlets.config.application import catch_config_error
+from traitlets.config.configurable import Configurable
+from traitlets import (
+    Unicode, 
+    List, 
+    Dict, 
+    default, 
+    validate
+)
+
+from jupyter_core.application import JupyterApp
+
+from jupyter_server.serverapp import ServerApp
+from jupyter_server.transutils import trans, _
+from jupyter_server.utils import url_path_join
+from jupyter_server.base.handlers import FileFindHandler
+
+
+class ExtensionApp(JupyterApp):
+    """A base class for writing configurable Jupyter Server extension applications.
+
+    These applications can be loaded using jupyter_server's 
+    extension load mechanism or launched using Jupyter's command line interface.
+    """
+    # Name of the extension
+    extension_name = Unicode(
+        "",
+        help="Name of extension."
+    )
+
+    @default("extension_name")
+    def _default_extension_name(self):
+        raise Exception("The extension must be given a `name`.")
+
+    @validate("extension_name")
+    def _default_extension_name(self, obj, value):
+        if isinstance(name, str):
+            # Validate that extension_name doesn't contain any invalid characters.
+            for char in [' ', '.', '+', '/']:
+                self.error(obj, value)
+            return value
+        self.error(obj, value) 
+
+    # Extension can configure the ServerApp from the command-line
+    classes = [
+        ServerApp
+    ]
+
+    static_file_path = List(Unicode(),
+        help="""paths to search for serving static files.
+        
+        This allows adding javascript/css to be available from the notebook server machine,
+        or overriding individual files in the IPython
+        """
+    ).tag(config=True)
+
+    template_path = List(Unicode(), 
+        help=_("""Paths to search for serving jinja templates.
+
+        Can be used to override templates from notebook.templates.""")
+    ).tag(config=True)
+
+    settings = Dict(
+        help=_("""Settings that will passed to the server.""")
+    )
+
+    handlers = List(
+        help=_("""Handlers appended to the server.""")
+    )
+
+    default_url = Unicode('/', config=True,
+        help=_("The default URL to redirect to from `/`")
+    )
+
+    def initialize_static_handler(self):
+        # Check to see if 
+        if len(self.static_file_path) > 0:
+            # Append the extension's static directory to server handlers.
+            static_url = url_path_join("/static", self.extension_name, "(.*)")
+
+            # Construct handler.
+            handler = (static_url, FileFindHandler, {'path': self.static_file_path})
+            self.handlers.append(handler)
+
+            # Add the file paths to webapp settings.
+            self.settings.update({
+                "{}_static_path".format(self.extension_name): self.static_file_path,
+                "{}_template_path".format(self.extension_name): self.template_path
+            })
+
+    def initialize_handlers(self):
+        """Override this method to append handlers to a Jupyter Server."""
+        pass
+
+    def initialize_templates(self):
+        """"""
+        pass
+
+    def initialize_settings(self):
+        """"""
+        pass
+
+    @staticmethod
+    def initialize_server():
+        """Add handlers to server."""
+        serverapp = ServerApp()
+        serverapp.initialize()
+        return serverapp
+
+    def initialize(self, serverapp, argv=None):
+        super(ExtensionApp, self).initialize(argv=argv)
+        self.serverapp = serverapp
+
+    def start(self, **kwargs):
+        # Start the server.
+        self.serverapp.start()
+
+    @classmethod
+    def launch_instance(cls, argv=None, **kwargs):
+        """Lśunch the ServerApp and Server Extension Application. 
+        
+        Properly orders the steps to initialize and start the server and extension.
+        """
+        # Initialize the server
+        serverapp = cls.initialize_server()
+
+        # Load the extension
+        extension = cls.load_jupyter_server_extension(serverapp, argv=argv, **kwargs)
+        
+        # Start the browser at this extensions default_url.
+        serverapp.default_url = extension.default_url
+
+        # Start the application.
+        extension.start()
+
+    @classmethod
+    def load_jupyter_server_extension(cls, serverapp, argv=None, **kwargs):
+        """Load this extension following the server extension loading mechanism."""
+        # Get webapp from the server.
+        webapp = serverapp.web_app
+        
+        # Create an instance and initialize extension.
+        extension = cls()
+        extension.initialize(serverapp, argv=argv)
+        extension.initialize_settings()
+        extension.initialize_handlers()
+        extension.initialize_static_handler()
+        extension.initialize_templates()
+
+        # Make extension settings accessible to handlers inside webapp settings.
+        webapp.settings.update(**extension.settings)
+        webapp.settings.update(**)
+
+        # Add handlers to serverapp.
+        webapp.add_handlers('.*$', extension.handlers)
+
+        return extension
---
+++ jupyter_server/extension/handler.py
@@ -0,0 +1,67 @@
+from jupyter_server.base.handlers import JupyterHandler, FileFindHandler
+from traitlets import Unicode, default
+
+
+class ExtensionHandler(JupyterHandler):
+    """Base class for Jupyter server extension handlers. 
+
+    Subclasses can serve static files behind a namespaced 
+    endpoint: "/static/<extension_name>/" 
+
+    This allows multiple extensions to serve static files under
+    their own namespace and avoid intercepting requests for 
+    other extensions. 
+    """
+    extension_name = Unicode(help="Name of the extenxsion")
+
+    @default('extension_name')
+    def _default_extension_name(self):
+        raise Exception("extension_name must be set in {}.".format(self.__class__))
+
+    @property
+    def static_url_prefix(self):
+        return "/static/{extension_name}/".format(
+            extension_name=self.extension_name)
+
+    @property
+    def static_path(self):
+        return self.settings['{}_static_path'.format(self.extension_name)]
+
+    def static_url(self, path, include_host=None, **kwargs):
+        """Returns a static URL for the given relative static file path.
+        This method requires you set the ``{extension_name}_static_path`` 
+        setting in your extension (which specifies the root directory 
+        of your static files).
+        This method returns a versioned url (by default appending
+        ``?v=<signature>``), which allows the static files to be
+        cached indefinitely.  This can be disabled by passing
+        ``include_version=False`` (in the default implementation;
+        other static file implementations are not required to support
+        this, but they may support other options).
+        By default this method returns URLs relative to the current
+        host, but if ``include_host`` is true the URL returned will be
+        absolute.  If this handler has an ``include_host`` attribute,
+        that value will be used as the default for all `static_url`
+        calls that do not pass ``include_host`` as a keyword argument.
+        """
+        self.require_setting("{}_static_path".format(self.extension_name), "static_url")
+
+        get_url = self.settings.get(
+            "static_handler_class", FileFindHandler
+        ).make_static_url
+
+        if include_host is None:
+            include_host = getattr(self, "include_host", False)
+
+        if include_host:
+            base = self.request.protocol + "://" + self.request.host
+        else:
+            base = ""
+
+        # Hijack settings dict to send extension templates to extension
+        # static directory.
+        settings = {
+            "static_path": self.static_path,
+            "static_url_prefix": self.static_url_prefix
+        }
+        return base + get_url(settings, path, **kwargs)
