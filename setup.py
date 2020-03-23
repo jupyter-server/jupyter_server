655d31a582d1c30b15f608135d4d1ce778beab4b
David Brochart <david.brochart@gmail.com>
2020-03-23 19:44:53 +0100

    Pin jupyter_client>=5.3.1

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ for more information.
         'ipython_genutils',
         'traitlets>=4.2.1',
         'jupyter_core>=4.4.0',
-        'jupyter_client>=6.1.0',
+        'jupyter_client>=5.3.1',
         'nbformat',
         'nbconvert<6',
         'ipykernel', # bless IPython kernel for now
