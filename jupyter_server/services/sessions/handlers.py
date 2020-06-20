6b7b1cdff8f4cdf541e4b43ca6d4f67373cff0ce
Ram Rachum <ram@rachum.com>
2020-06-20 11:43:34 +0300

    Fix exception causes all over the codebase

--- jupyter_server/services/sessions/handlers.py
+++ jupyter_server/services/sessions/handlers.py
@@ -42,13 +42,13 @@ class SessionRootHandler(APIHandler):
 
         try:
             path = model['path']
-        except KeyError:
-            raise web.HTTPError(400, "Missing field in JSON data: path")
+        except KeyError as e:
+            raise web.HTTPError(400, "Missing field in JSON data: path") from e
 
         try:
             mtype = model['type']
-        except KeyError:
-            raise web.HTTPError(400, "Missing field in JSON data: type")
+        except KeyError as e:
+            raise web.HTTPError(400, "Missing field in JSON data: type") from e
 
         name = model.get('name', None)
         kernel = model.get('kernel', {})
@@ -148,9 +148,9 @@ class SessionHandler(APIHandler):
         sm = self.session_manager
         try:
             await sm.delete_session(session_id)
-        except KeyError:
+        except KeyError as e:
             # the kernel was deleted but the session wasn't!
-            raise web.HTTPError(410, "Kernel deleted before session")
+            raise web.HTTPError(410, "Kernel deleted before session") from e
         self.set_status(204)
         self.finish()
 
