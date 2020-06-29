bd592a41cca763f85da40063dd4b1caae12a330b
Hadrien Mary <hadrien.mary@gmail.com>
2020-06-29 16:51:42 -0400

    Add DirectoryHandler and ExtractDirectoryHandler + tests

--- jupyter_server/files/handlers.py
+++ jupyter_server/files/handlers.py
@@ -3,11 +3,17 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
+import os
+import pathlib
 import mimetypes
 import json
+import zipfile
+import tarfile
 from base64 import decodebytes
 from tornado import web
+from tornado import ioloop
 from jupyter_server.base.handlers import JupyterHandler
+from jupyter_server.utils import url2path
 
 
 class FilesHandler(JupyterHandler):
@@ -23,8 +29,10 @@ class FilesHandler(JupyterHandler):
     def content_security_policy(self):
         # In case we're serving HTML/SVG, confine any Javascript to a unique
         # origin so it can't interact with the notebook server.
-        return super(FilesHandler, self).content_security_policy + \
-               "; sandbox allow-scripts"
+        return (
+            super(FilesHandler, self).content_security_policy
+            + "; sandbox allow-scripts"
+        )
 
     @web.authenticated
     def head(self, path):
@@ -38,41 +46,229 @@ class FilesHandler(JupyterHandler):
             self.log.info("Refusing to serve hidden file, via 404 Error")
             raise web.HTTPError(404)
 
-        path = path.strip('/')
-        if '/' in path:
-            _, name = path.rsplit('/', 1)
+        path = path.strip("/")
+        if "/" in path:
+            _, name = path.rsplit("/", 1)
         else:
             name = path
 
-        model = await cm.get(path, type='file', content=include_body)
+        model = await cm.get(path, type="file", content=include_body)
 
         if self.get_argument("download", False):
             self.set_attachment_header(name)
 
         # get mimetype from filename
-        if name.lower().endswith('.ipynb'):
-            self.set_header('Content-Type', 'application/x-ipynb+json')
+        if name.lower().endswith(".ipynb"):
+            self.set_header("Content-Type", "application/x-ipynb+json")
         else:
             cur_mime = mimetypes.guess_type(name)[0]
-            if cur_mime == 'text/plain':
-                self.set_header('Content-Type', 'text/plain; charset=UTF-8')
+            if cur_mime == "text/plain":
+                self.set_header("Content-Type", "text/plain; charset=UTF-8")
             elif cur_mime is not None:
-                self.set_header('Content-Type', cur_mime)
+                self.set_header("Content-Type", cur_mime)
             else:
-                if model['format'] == 'base64':
-                    self.set_header('Content-Type', 'application/octet-stream')
+                if model["format"] == "base64":
+                    self.set_header("Content-Type", "application/octet-stream")
                 else:
-                    self.set_header('Content-Type', 'text/plain; charset=UTF-8')
+                    self.set_header("Content-Type", "text/plain; charset=UTF-8")
 
         if include_body:
-            if model['format'] == 'base64':
-                b64_bytes = model['content'].encode('ascii')
+            if model["format"] == "base64":
+                b64_bytes = model["content"].encode("ascii")
                 self.write(decodebytes(b64_bytes))
-            elif model['format'] == 'json':
-                self.write(json.dumps(model['content']))
+            elif model["format"] == "json":
+                self.write(json.dumps(model["content"]))
             else:
-                self.write(model['content'])
+                self.write(model["content"])
             self.flush()
+
+
+# The delay in ms at which we send the chunk of data
+# to the client.
+ARCHIVE_DOWNLOAD_FLUSH_DELAY = 100
+SUPPORTED_FORMAT = [
+    "zip",
+    "tgz",
+    "tar.gz",
+    "tbz",
+    "tbz2",
+    "tar.bz",
+    "tar.bz2",
+    "txz",
+    "tar.xz",
+]
+
+
+class ArchiveStream:
+    def __init__(self, handler):
+        self.handler = handler
+        self.position = 0
+
+    def write(self, data):
+        self.position += len(data)
+        self.handler.write(data)
+        del data
+
+    def tell(self):
+        return self.position
+
+    def flush(self):
+        # Note: Flushing is done elsewhere, in the main thread
+        # because `write()` is called in a background thread.
+        # self.handler.flush()
+        pass
+
+
+def make_writer(handler, archive_format="zip"):
+    fileobj = ArchiveStream(handler)
+
+    if archive_format == "zip":
+        archive_file = zipfile.ZipFile(
+            fileobj, mode="w", compression=zipfile.ZIP_DEFLATED
+        )
+        archive_file.add = archive_file.write
+    elif archive_format in ["tgz", "tar.gz"]:
+        archive_file = tarfile.open(fileobj=fileobj, mode="w|gz")
+    elif archive_format in ["tbz", "tbz2", "tar.bz", "tar.bz2"]:
+        archive_file = tarfile.open(fileobj=fileobj, mode="w|bz2")
+    elif archive_format in ["txz", "tar.xz"]:
+        archive_file = tarfile.open(fileobj=fileobj, mode="w|xz")
+    else:
+        raise ValueError("'{}' is not a valid archive format.".format(archive_format))
+    return archive_file
+
+
+def make_reader(archive_path):
+
+    archive_format = "".join(archive_path.suffixes)[1:]
+
+    if archive_format == "zip":
+        archive_file = zipfile.ZipFile(archive_path, mode="r")
+    elif archive_format in ["tgz", "tar.gz"]:
+        archive_file = tarfile.open(archive_path, mode="r|gz")
+    elif archive_format in ["tbz", "tbz2", "tar.bz", "tar.bz2"]:
+        archive_file = tarfile.open(archive_path, mode="r|bz2")
+    elif archive_format in ["txz", "tar.xz"]:
+        archive_file = tarfile.open(archive_path, mode="r|xz")
+    else:
+        raise ValueError("'{}' is not a valid archive format.".format(archive_format))
+    return archive_file
+
+
+class DirectoryHandler(JupyterHandler):
+    @web.authenticated
+    async def get(self, archive_path, include_body=False):
+
+        # /directories/ requests must originate from the same site
+        self.check_xsrf_cookie()
+        cm = self.contents_manager
+
+        if cm.is_hidden(archive_path) and not cm.allow_hidden:
+            self.log.info("Refusing to serve hidden file, via 404 Error")
+            raise web.HTTPError(404)
+
+        archive_token = self.get_argument("archiveToken")
+        archive_format = self.get_argument("archiveFormat", "zip")
+        if archive_format not in SUPPORTED_FORMAT:
+            self.log.error("Unsupported format {}.".format(archive_format))
+            raise web.HTTPError(404)
+
+        root_dir = pathlib.Path(cm.root_dir)
+        archive_path = root_dir / url2path(archive_path)
+        archive_filename = archive_path.with_suffix(".{}".format(archive_format)).name
+
+        self.log.info(
+            "Prepare {} for archiving and downloading.".format(archive_filename)
+        )
+        self.set_header("content-type", "application/octet-stream")
+        self.set_header("cache-control", "no-cache")
+        self.set_header(
+            "content-disposition", "attachment; filename={}".format(archive_filename)
+        )
+
+        self.canceled = False
+
+        # The original extension is using `ioloop.PeriodicCallback` and `ioloop.IOLoop.current().run_in_executor`
+        # but it does not seem to work here (500 internal HTTP error).
+        # This trategy is nice when compressing very large directory. Any idea how to integrate that is welcome.
+
+        # self.flush_cb = ioloop.PeriodicCallback(
+        #     self.flush, ARCHIVE_DOWNLOAD_FLUSH_DELAY
+        # )
+        # self.flush_cb.start()
+
+        self.archive_and_download(archive_path, archive_format, archive_token)
+
+        # args = (archive_path, archive_format, archive_token)
+        # yield ioloop.IOLoop.current().run_in_executor(
+        #     None, self.archive_and_download, *args
+        # )
+
+        if self.canceled:
+            self.log.info("Download canceled.")
+        else:
+            self.flush()
+            self.log.info("Finished downloading {}.".format(archive_filename))
+
+        self.set_cookie("archiveToken", archive_token)
+        # self.flush_cb.stop()
+        self.finish()
+
+    def archive_and_download(self, archive_path, archive_format, archive_token):
+
+        with make_writer(self, archive_format) as archive:
+            prefix = len(str(archive_path.parent)) + len(os.path.sep)
+            for root, _, files in os.walk(archive_path):
+                for file_ in files:
+                    file_name = os.path.join(root, file_)
+                    if not self.canceled:
+                        self.log.debug("{}\n".format(file_name))
+                        archive.add(file_name, os.path.join(root[prefix:], file_))
+                    else:
+                        break
+
+    def on_connection_close(self):
+        super().on_connection_close()
+        self.canceled = True
+        # self.flush_cb.stop()
+
+
+class ExtractDirectoryHandler(JupyterHandler):
+    @web.authenticated
+    def get(self, archive_path, include_body=False):
+
+        # /extract-archive/ requests must originate from the same site
+        self.check_xsrf_cookie()
+        cm = self.contents_manager
+
+        if cm.is_hidden(archive_path) and not cm.allow_hidden:
+            self.log.info("Refusing to serve hidden file, via 404 Error")
+            raise web.HTTPError(404)
+
+        root_dir = pathlib.Path(cm.root_dir)
+        archive_path = root_dir / url2path(archive_path)
+
+        # yield ioloop.IOLoop.current().run_in_executor(
+        #     None, self.extract_archive, archive_path
+        # )
+        self.extract_archive(archive_path)
+
+        self.finish()
+
+    def extract_archive(self, archive_path):
+
+        archive_destination = archive_path.parent
+        self.log.info(
+            "Begin extraction of {} to {}.".format(archive_path, archive_destination)
+        )
+
+        archive_reader = make_reader(archive_path)
+        with archive_reader as archive:
+            archive.extractall(archive_destination)
+
+        self.log.info(
+            "Finished extracting {} to {}.".format(archive_path, archive_destination)
+        )
 
 
 default_handlers = []
--- tests/test_files.py
+++ tests/test_files.py
@@ -1,85 +1,68 @@
 import os
+import io
+import zipfile
+import tarfile
+import pathlib
+import shutil
 import pytest
 import tornado
 
 from .utils import expected_http_error
 
 from nbformat import writes
-from nbformat.v4 import (new_notebook,
-                         new_markdown_cell, new_code_cell,
-                         new_output)
+from nbformat.v4 import new_notebook, new_markdown_cell, new_code_cell, new_output
 
 
 async def test_hidden_files(fetch, serverapp, root_dir):
     not_hidden = [
-        u'å b',
-        u'å b/ç. d',
+        u"å b",
+        u"å b/ç. d",
     ]
     hidden = [
-        u'.å b',
-        u'å b/.ç d',
+        u".å b",
+        u"å b/.ç d",
     ]
     dirs = not_hidden + hidden
 
     for d in dirs:
-        path  = root_dir / d.replace('/', os.sep)
+        path = root_dir / d.replace("/", os.sep)
         path.mkdir(parents=True, exist_ok=True)
-        path.joinpath('foo').write_text('foo')
-        path.joinpath('.foo').write_text('.foo')
-
+        path.joinpath("foo").write_text("foo")
+        path.joinpath(".foo").write_text(".foo")
 
     for d in not_hidden:
-        path = root_dir / d.replace('/', os.sep)
+        path = root_dir / d.replace("/", os.sep)
 
-        r = await fetch(
-            'files', d, 'foo',
-            method='GET'
-        )
-        assert r.body.decode() == 'foo'
+        r = await fetch("files", d, "foo", method="GET")
+        assert r.body.decode() == "foo"
 
         with pytest.raises(tornado.httpclient.HTTPClientError) as e:
-            r = await fetch(
-                'files', d, '.foo',
-                method='GET'
-            )
+            r = await fetch("files", d, ".foo", method="GET")
         assert expected_http_error(e, 404)
 
-
     for d in hidden:
-        path = root_dir / d.replace('/', os.sep)
-        for foo in ('foo', '.foo'):
+        path = root_dir / d.replace("/", os.sep)
+        for foo in ("foo", ".foo"):
             with pytest.raises(tornado.httpclient.HTTPClientError) as e:
-                r = await fetch(
-                    'files', d, foo,
-                    method='GET'
-                )
+                r = await fetch("files", d, foo, method="GET")
             assert expected_http_error(e, 404)
 
     serverapp.contents_manager.allow_hidden = True
 
     for d in not_hidden:
-        path = root_dir / d.replace('/', os.sep)
+        path = root_dir / d.replace("/", os.sep)
 
-        r = await fetch(
-            'files', d, 'foo',
-            method='GET'
-        )
-        assert r.body.decode() == 'foo'
+        r = await fetch("files", d, "foo", method="GET")
+        assert r.body.decode() == "foo"
 
-        r = await fetch(
-            'files', d, '.foo',
-            method='GET'
-        )
-        assert r.body.decode() == '.foo'
+        r = await fetch("files", d, ".foo", method="GET")
+        assert r.body.decode() == ".foo"
 
         for d in hidden:
-            path = root_dir / d.replace('/', os.sep)
+            path = root_dir / d.replace("/", os.sep)
 
-            for foo in ('foo', '.foo'):
-                r = await fetch(
-                    'files', d, foo,
-                    method='GET'
-                )
+            for foo in ("foo", ".foo"):
+                r = await fetch("files", d, foo, method="GET")
                 assert r.body.decode() == foo
 
 
@@ -87,73 +70,56 @@ async def test_contents_manager(fetch, serverapp, root_dir):
     "make sure ContentsManager returns right files (ipynb, bin, txt)."
     nb = new_notebook(
         cells=[
-            new_markdown_cell(u'Created by test ³'),
-            new_code_cell("print(2*6)", outputs=[
-                new_output("stream", text="12"),
-            ])
+            new_markdown_cell(u"Created by test ³"),
+            new_code_cell("print(2*6)", outputs=[new_output("stream", text="12"),]),
         ]
     )
-    root_dir.joinpath('testnb.ipynb').write_text(writes(nb, version=4), encoding='utf-8')
-    root_dir.joinpath('test.bin').write_bytes(b'\xff' + os.urandom(5))
-    root_dir.joinpath('test.txt').write_text('foobar')
-
-    r = await fetch(
-        'files/testnb.ipynb',
-        method='GET'
+    root_dir.joinpath("testnb.ipynb").write_text(
+        writes(nb, version=4), encoding="utf-8"
     )
-    assert r.code == 200
-    assert 'print(2*6)' in r.body.decode('utf-8')
+    root_dir.joinpath("test.bin").write_bytes(b"\xff" + os.urandom(5))
+    root_dir.joinpath("test.txt").write_text("foobar")
 
-    r = await fetch(
-        'files/test.bin',
-        method='GET'
-    )
+    r = await fetch("files/testnb.ipynb", method="GET")
     assert r.code == 200
-    assert r.headers['content-type'] == 'application/octet-stream'
-    assert r.body[:1] == b'\xff'
+    assert "print(2*6)" in r.body.decode("utf-8")
+
+    r = await fetch("files/test.bin", method="GET")
+    assert r.code == 200
+    assert r.headers["content-type"] == "application/octet-stream"
+    assert r.body[:1] == b"\xff"
     assert len(r.body) == 6
 
-    r = await fetch(
-        'files/test.txt',
-        method='GET'
-    )
+    r = await fetch("files/test.txt", method="GET")
     assert r.code == 200
-    assert r.headers['content-type'] == 'text/plain; charset=UTF-8'
-    assert r.body.decode() == 'foobar'
+    assert r.headers["content-type"] == "text/plain; charset=UTF-8"
+    assert r.body.decode() == "foobar"
 
 
 async def test_download(fetch, serverapp, root_dir):
-    text = 'hello'
-    root_dir.joinpath('test.txt').write_text(text)
+    text = "hello"
+    root_dir.joinpath("test.txt").write_text(text)
 
-    r = await fetch(
-        'files', 'test.txt',
-        method='GET'
-    )
-    disposition = r.headers.get('Content-Disposition', '')
-    assert 'attachment' not in disposition
+    r = await fetch("files", "test.txt", method="GET")
+    disposition = r.headers.get("Content-Disposition", "")
+    assert "attachment" not in disposition
 
-    r = await fetch(
-        'files', 'test.txt',
-        method='GET',
-        params={'download': True}
-    )
-    disposition = r.headers.get('Content-Disposition', '')
-    assert 'attachment' in disposition
+    r = await fetch("files", "test.txt", method="GET", params={"download": True})
+    disposition = r.headers.get("Content-Disposition", "")
+    assert "attachment" in disposition
     assert "filename*=utf-8''test.txt" in disposition
 
 
 async def test_old_files_redirect(fetch, serverapp, root_dir):
     """pre-2.0 'files/' prefixed links are properly redirected"""
-    root_dir.joinpath('files').mkdir(parents=True, exist_ok=True)
-    root_dir.joinpath('sub', 'files').mkdir(parents=True, exist_ok=True)
+    root_dir.joinpath("files").mkdir(parents=True, exist_ok=True)
+    root_dir.joinpath("sub", "files").mkdir(parents=True, exist_ok=True)
 
-
-    for prefix in ('', 'sub'):
-        root_dir.joinpath(prefix, 'files', 'f1.txt').write_text(prefix + '/files/f1')
-        root_dir.joinpath(prefix, 'files', 'f2.txt').write_text(prefix + '/files/f2')
-        root_dir.joinpath(prefix, 'f2.txt').write_text(prefix + '/f2')
-        root_dir.joinpath(prefix, 'f3.txt').write_text(prefix + '/f3')
+    for prefix in ("", "sub"):
+        root_dir.joinpath(prefix, "files", "f1.txt").write_text(prefix + "/files/f1")
+        root_dir.joinpath(prefix, "files", "f2.txt").write_text(prefix + "/files/f2")
+        root_dir.joinpath(prefix, "f2.txt").write_text(prefix + "/f2")
+        root_dir.joinpath(prefix, "f3.txt").write_text(prefix + "/f3")
 
     # These depend on the tree handlers
     #
@@ -172,3 +138,109 @@ async def test_old_files_redirect(fetch, serverapp, root_dir):
     #     disposition = r.headers.get('Content-Disposition', '')
     #     self.assertIn('attachment', disposition)
     #     self.assertIn("filename*=utf-8''test.txt", disposition)
+
+
+async def test_download_directory(fetch, serverapp, root_dir):
+    # Create a dummy directory.
+    root_dir = pathlib.Path(root_dir)
+    archive_dir_path = root_dir / "download-archive-dir"
+    archive_dir_path.mkdir()
+    with open(archive_dir_path / "test1.txt", "w") as f:
+        f.write("hello1")
+    with open(archive_dir_path / "test2.txt", "w") as f:
+        f.write("hello2")
+    with open(archive_dir_path / "test3.md", "w") as f:
+        f.write("hello3")
+
+    # Try to download the created folder.
+    file_lists = {
+        "download-archive-dir/test2.txt",
+        "download-archive-dir/test1.txt",
+        "download-archive-dir/test3.md",
+    }
+
+    archive_formats = {
+        "zip": "r",
+        "tgz": "r|gz",
+        "tar.gz": "r|gz",
+        "tbz": "r|bz2",
+        "tbz2": "r|bz2",
+        "tar.bz": "r|bz2",
+        "tar.bz2": "r|bz2",
+        "txz": "r|xz",
+        "tar.xz": "r|xz",
+    }
+
+    archive_token = 59487596
+
+    for archive_format, mode in archive_formats.items():
+        params = dict(archiveToken=archive_token, archiveFormat=archive_format)
+        dir_path = str(archive_dir_path.relative_to(root_dir))
+        r = await fetch("directories", dir_path, method="GET", params=params)
+        assert r.code == 200
+        assert r.headers.get("content-type") == "application/octet-stream"
+        assert r.headers.get("cache-control") == "no-cache"
+        if archive_format == "zip":
+            with zipfile.ZipFile(io.BytesIO(r.body), mode=mode) as zf:
+                assert set(zf.namelist()) == file_lists
+        else:
+            with tarfile.open(fileobj=io.BytesIO(r.body), mode=mode) as tf:
+                assert set(map(lambda m: m.name, tf.getmembers())) == file_lists
+
+
+async def test_extract_directory(fetch, serverapp, root_dir):
+
+    format_mode = {
+        "zip": "w",
+        "tgz": "w|gz",
+        "tar.gz": "w|gz",
+        "tbz": "w|bz2",
+        "tbz2": "w|bz2",
+        "tar.bz": "w|bz2",
+        "tar.bz2": "w|bz2",
+        "txz": "w|xz",
+        "tar.xz": "w|xz",
+    }
+
+    for archive_format, mode in format_mode.items():
+
+        # Create a dummy directory.
+        root_dir = pathlib.Path(root_dir)
+        archive_dir_path = root_dir / "extract-archive-dir"
+        archive_dir_path.mkdir()
+        with open(archive_dir_path / "extract-test1.txt", "w") as f:
+            f.write("hello1")
+        with open(archive_dir_path / "extract-test2.txt", "w") as f:
+            f.write("hello2")
+        with open(archive_dir_path / "extract-test3.md", "w") as f:
+            f.write("hello3")
+
+        # Make an archive
+        archive_path = archive_dir_path.with_suffix("." + archive_format)
+        if archive_format == "zip":
+            with zipfile.ZipFile(archive_path, mode=mode) as writer:
+                for file_path in archive_dir_path.rglob("*"):
+                    if file_path.is_file():
+                        writer.write(file_path, file_path.relative_to(root_dir))
+        else:
+            with tarfile.open(str(archive_path), mode=mode) as writer:
+                for file_path in archive_dir_path.rglob("*"):
+                    if file_path.is_file():
+                        writer.add(str(file_path), str(file_path.relative_to(root_dir)))
+
+        # Remove the directory
+        shutil.rmtree(archive_dir_path)
+
+        # REST call to extract the archive
+        relative_archive_path = str(archive_path.relative_to(root_dir))
+        print(archive_path)
+        r = await fetch("extract-directories", relative_archive_path, method="GET")
+
+        assert r.code == 200
+        assert archive_dir_path.is_dir()
+
+        n_files = len(list(archive_dir_path.glob("*")))
+        assert n_files == 3
+
+        # Remove the directory after extraction
+        shutil.rmtree(archive_dir_path)
