20586f8570a1fdda0ff0335abb858cc993e23d35
Steven Silvester <stslve@amazon.com>
2020-08-31 16:47:06 -0500

    fix tests

--- tests/services/kernels/test_config.py
+++ tests/services/kernels/test_config.py
@@ -19,20 +19,8 @@ def test_config(serverapp):
     assert serverapp.kernel_manager.allowed_message_types == ['kernel_info_request']
 
 
-@pytest.mark.skipif(
-    sys.version_info < (3, 6),
-    reason="Kernel manager is AsyncMappingKernelManager, Python version < 3.6"
-)
 async def test_async_kernel_manager(configurable_serverapp):
     argv = ['--ServerApp.kernel_manager_class=jupyter_server.services.kernels.kernelmanager.AsyncMappingKernelManager']
     app = configurable_serverapp(argv=argv)
     assert isinstance(app.kernel_manager, AsyncMappingKernelManager)
 
-
-@pytest.mark.parametrize(
-    "args",
-    [['--ServerApp.kernel_manager_class=jupyter_server.services.kernels.kernelmanager.AsyncMappingKernelManager']]
-)
-async def test_async_kernel_manager_not_available_py35(configurable_serverapp, args):
-    with pytest.raises(ValueError):
-        app = configurable_serverapp(argv=args)
