e4784ba816b4adeb69575d45630a55bb346c38c1
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2021-05-01 10:14:54 +0000

    Publish 1.7.0a0
    SHA256 hashes:
    
    dist/jupyter_server-1.7.0a0-py3-none-any.whl: b7813555b6a532806fbb7c51e8c77cc1bea2d16d27e6f21ca9dac19a25aec0f8
    
    dist/jupyter_server-1.7.0a0.tar.gz: ab5a211c57be3bee20d32976a72ca9de82c4d9a7d72033ab5f807a2960c5f93c

--- jupyter_server/_version.py
+++ jupyter_server/_version.py
@@ -5,5 +5,5 @@ store the current version info of the server.
 from jupyter_packaging import get_version_info
 
 # Version string must appear intact for tbump versioning
-__version__ = '1.7.0.dev0'
+__version__ = '1.7.0a0'
 version_info = get_version_info(__version__)
