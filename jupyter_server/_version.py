48cf6a5d755989e3cd64644d805371a01cc0ff2b
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2021-05-05 10:52:26 +0000

    Bump to 1.8.0.dev0

--- jupyter_server/_version.py
+++ jupyter_server/_version.py
@@ -5,5 +5,5 @@ store the current version info of the server.
 from jupyter_packaging import get_version_info
 
 # Version string must appear intact for tbump versioning
-__version__ = '1.7.0a1'
+__version__ = '1.8.0.dev0'
 version_info = get_version_info(__version__)
