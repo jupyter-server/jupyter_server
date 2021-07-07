81d46e3255ac2093bb8c30d386d9244fb63fb98f
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2021-07-07 16:56:36 +0100

    extensions: allow extensions in namespace packages (#523)

--- jupyter_server/tests/test_utils.py
+++ jupyter_server/tests/test_utils.py
@@ -1,19 +1,25 @@
+from pathlib import Path
+import sys
+
 import pytest
 
 from traitlets.tests.utils import check_help_all_output
-from jupyter_server.utils import url_escape, url_unescape
+from jupyter_server.utils import (
+    url_escape,
+    url_unescape,
+    is_namespace_package
+)
 
 
 def test_help_output():
     check_help_all_output('jupyter_server')
 
 
-
 @pytest.mark.parametrize(
     'unescaped,escaped',
     [
         (
-            '/this is a test/for spaces/', 
+            '/this is a test/for spaces/',
             '/this%20is%20a%20test/for%20spaces/'
         ),
         (
@@ -37,3 +43,30 @@ def test_url_escaping(unescaped, escaped):
     # Test unescaping.
     path = url_unescape(escaped)
     assert path == unescaped
+
+
+@pytest.fixture
+def namespace_package_test(monkeypatch):
+    """Adds a blank namespace package into the PYTHONPATH for testing.
+
+    Yields the name of the importable namespace.
+    """
+    monkeypatch.setattr(
+        sys,
+        'path',
+        [
+            str(Path(__file__).parent / 'namespace-package-test'),
+            *sys.path
+        ]
+    )
+    yield 'test_namespace'
+
+
+def test_is_namespace_package(namespace_package_test):
+    # returns True if it is a namespace package
+    assert is_namespace_package(namespace_package_test)
+    # returns False if it isn't a namespace package
+    assert not is_namespace_package('sys')
+    assert not is_namespace_package('jupyter_server')
+    # returns None if it isn't importable
+    assert is_namespace_package('not_a_python_namespace') is None
