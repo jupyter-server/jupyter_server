4450cd27bd5e40e44370e554b72d3f80fdd8d40f
martinRenou <martin.renou@gmail.com>
2021-09-06 10:34:33 +0200

    Shutdown terminals on api/shutdown

--- jupyter_server/services/shutdown.py
+++ jupyter_server/services/shutdown.py
@@ -11,6 +11,7 @@ class ShutdownHandler(JupyterHandler):
     async def post(self):
         self.log.info("Shutting down on /api/shutdown request.")
 
+        await self.terminal_manager.terminate_all()
         await self.kernel_manager.shutdown_all()
 
         ioloop.IOLoop.current().stop()
