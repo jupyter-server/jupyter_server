968a637fa49c4f62a24f2c0004c8c00bc507a458
Zach Sailer <zsailer@apple.com>
2021-11-01 09:00:07 -0700

    make nbconvert API test less dependent on nbconvert versions

--- jupyter_server/tests/services/nbconvert/test_api.py
+++ jupyter_server/tests/services/nbconvert/test_api.py
@@ -4,7 +4,11 @@ import json
 async def test_list_formats(jp_fetch):
     r = await jp_fetch("api", "nbconvert", method="GET")
     formats = json.loads(r.body.decode())
+    # Verify the type of the response.
     assert isinstance(formats, dict)
-    assert "python" in formats
-    assert "html" in formats
-    assert formats["python"]["output_mimetype"] == "text/x-python"
+    # Verify that all returned formats have an
+    # output mimetype defined.
+    required_keys_present = []
+    for name, data in formats.items():
+        required_keys_present.append("output_mimetype" in data)
+    assert all(required_keys_present), "All returned formats must have a `output_mimetype` key."
