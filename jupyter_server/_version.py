f9e19f2b6374e0dae0d04e997cfc6f1b41e46b22
Steven Silvester <steven.silvester@ieee.org>
2021-11-27 19:52:33 -0600

    use tbump fields

--- jupyter_server/_version.py
+++ jupyter_server/_version.py
@@ -2,15 +2,5 @@
 store the current version info of the server.
 
 """
-import re
-
-# Version string must appear intact for tbump versioning
-__version__ = "1.12.1"
-
-# Build up version_info tuple for backwards compatibility
-pattern = r"(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)"
-match = re.match(pattern, __version__)
-parts = [int(match[part]) for part in ["major", "minor", "patch"]]
-if match["rest"]:
-    parts.append(match["rest"])
-version_info = tuple(parts)
+version_info = (1, 12, 1, "", "")
+__version__ = ".".join(map(str, version_info[:3])) + "".join(version_info[3:])
