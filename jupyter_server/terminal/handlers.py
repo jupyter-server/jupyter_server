4f4502a84b46dcf43349adbfa78f9cf4fc440de5
jizhongsheng <jizhongsheng@qianxin.com>
2021-12-02 22:21:54 +0800

    Record the first output to identify the terminal return

--- jupyter_server/terminal/handlers.py
+++ jupyter_server/terminal/handlers.py
@@ -2,6 +2,8 @@
 """Tornado handlers for the terminal emulator."""
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
+import json
+
 import terminado
 from tornado import web
 
@@ -11,6 +13,10 @@ from ..base.zmqhandlers import WebSocketMixin
 
 
 class TermSocket(WebSocketMixin, JupyterHandler, terminado.TermSocket):
+    def initialize(self, *args, **kwargs):
+        super(TermSocket, self).initialize(*args, **kwargs)
+        self._first_stdout = ''
+
     def origin_check(self):
         """Terminado adds redundant origin_check
         Tornado already calls check_origin, so don't do anything here.
@@ -31,8 +37,15 @@ class TermSocket(WebSocketMixin, JupyterHandler, terminado.TermSocket):
 
     def write_message(self, message, binary=False):
         super(TermSocket, self).write_message(message, binary=binary)
-        self._update_activity()
-        if message != '["stdout", "\\r\\n"]':
+        message_seg = json.loads(message)
+        if not self._first_stdout and message_seg[0] == 'stdout':
+            # Record the first output to identify the terminal return
+            # It works well for jupyterhub-singleuser and should also work for other debian-based mirrors
+            # fixme: May fail if terminal is not properly separated with ':' or change user after connect
+            #        (Any change to the user, hostname or environment may render it invalid)
+            self._first_stdout = message_seg[1].split(':')[0].lstrip()
+            self.log.debug(f'take "{self._first_stdout}" as terminal returned')
+        if isinstance(message_seg[1], str) and message_seg[1].lstrip().startswith(self._first_stdout):
             self._set_state_idle()
 
     def _update_activity(self):
@@ -42,11 +55,10 @@ class TermSocket(WebSocketMixin, JupyterHandler, terminado.TermSocket):
             self.terminal_manager.terminals[self.term_name].last_activity = utcnow()
 
     def _set_state_busy(self):
-        self.log.debug('set terminal execution_state as busy')
         if self.term_name in self.terminal_manager.terminals:
             self.terminal_manager.terminals[self.term_name].execution_state = 'busy'
 
     def _set_state_idle(self):
-        self.log.debug('set terminal execution_state as idle')
         if self.term_name in self.terminal_manager.terminals:
+            self.log.debug('set terminal execution_state as idle')
             self.terminal_manager.terminals[self.term_name].execution_state = 'idle'
