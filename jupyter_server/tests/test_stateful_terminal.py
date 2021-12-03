3c0688a273555d04633d123213d5ecb453132472
jizhongsheng <jizhongsheng@qianxin.com>
2021-12-03 16:26:55 +0800

    improve stateful terminal test

--- jupyter_server/tests/test_stateful_terminal.py
+++ jupyter_server/tests/test_stateful_terminal.py
@@ -76,12 +76,11 @@ async def test_set_idle_disconnect(jp_fetch, jp_ws_fetch, jp_cleanup_subprocesse
     setup = ["set_size", 0, 0, 80, 32]
     await ws.write_message(json.dumps(setup))
     await ws.read_message()
-    sleep_3_msg = ['stdin', "python -c 'import time;time.sleep(3)'\r\n"]
-    await ws.write_message(json.dumps(sleep_3_msg))
+    sleep_1_msg = ['stdin', "python -c 'import time;time.sleep(1)'\r\n"]
+    await ws.write_message(json.dumps(sleep_1_msg))
     ws.close()
-    await asyncio.sleep(1)
-    assert not jp_serverapp.web_app.settings["terminal_manager"].terminals[term_1].clients
     assert jp_serverapp.web_app.settings["terminal_manager"].terminals[term_1].execution_state == 'busy'
-    await asyncio.sleep(3)
+    await asyncio.sleep(2)
+    assert not jp_serverapp.web_app.settings["terminal_manager"].terminals[term_1].clients
     assert jp_serverapp.web_app.settings["terminal_manager"].terminals[term_1].execution_state == 'idle'
     await jp_cleanup_subprocesses()
