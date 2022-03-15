427ce75ac627d498f37f3e2ee61375f061e96f50
Steven Silvester <steven.silvester@ieee.org>
2022-03-15 10:37:57 -0500

    Bump to 1.15.4

--- jupyter_server/_version.py
+++ jupyter_server/_version.py
@@ -2,5 +2,5 @@
 store the current version info of the server.
 
 """
-version_info = (1, 15, 3, "", "")
+version_info = (1, 15, 4, "", "")
 __version__ = ".".join(map(str, version_info[:3])) + "".join(version_info[3:])
