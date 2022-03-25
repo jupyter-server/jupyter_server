9177f11e61c7527a8d495d828949ac45b78febed
David Leen <daleen@gmail.com>
2022-03-25 07:21:09 -0700

    Handle importstring pre/post save hooks (#754)

--- tests/services/contents/test_config.py
+++ tests/services/contents/test_config.py
@@ -18,6 +18,30 @@ def test_config_did_something(jp_server_config, jp_serverapp):
     )
 
 
+def example_pre_save_hook():
+    pass
+
+
+def example_post_save_hook():
+    pass
+
+
+@pytest.mark.parametrize(
+    "jp_server_config",
+    [
+        {
+            "ContentsManager": {
+                "pre_save_hook": "tests.services.contents.test_config.example_pre_save_hook",
+                "post_save_hook": "tests.services.contents.test_config.example_post_save_hook",
+            },
+        }
+    ],
+)
+def test_pre_post_save_hook_config(jp_serverapp, jp_server_config):
+    assert jp_serverapp.contents_manager.pre_save_hook.__name__ == "example_pre_save_hook"
+    assert jp_serverapp.contents_manager.post_save_hook.__name__ == "example_post_save_hook"
+
+
 async def test_async_contents_manager(jp_configurable_serverapp):
     config = {"ContentsManager": {"checkpoints_class": AsyncCheckpoints}}
     argv = [
