f28d2f7c9d3a2502f0ea2e108f800f5a5a8933c5
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-03-29 23:29:34 +0000

    Bump to 1.16.1.dev0

--- jupyter_server/_version.py
+++ jupyter_server/_version.py
@@ -2,5 +2,5 @@
 store the current version info of the server.
 
 """
-version_info = (1, 16, 0, "", "")
+version_info = (1, 16, 1, ".dev", "0")
 __version__ = ".".join(map(str, version_info[:3])) + "".join(version_info[3:])
