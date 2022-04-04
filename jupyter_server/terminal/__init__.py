c0dfbf0a0b6bf7c1379859c684bc9b56c7130061
Steven Silvester <steven.silvester@ieee.org>
2022-04-04 05:05:11 -0500

    add import warning on shim

--- jupyter_server/terminal/__init__.py
+++ jupyter_server/terminal/__init__.py
@@ -1,4 +1,12 @@
+import warnings
+
 # Shims
 from jupyter_server_terminals import api_handlers, initialize  # noqa
 from jupyter_server_terminals.handlers import TermSocket  # noqa
 from jupyter_server_terminals.terminalmanager import TerminalManager  # noqa
+
+warnings.warn(
+    "Terminals support has moved to `jupyter_server_terminals`",
+    DeprecationWarning,
+    stacklevel=2,
+)
