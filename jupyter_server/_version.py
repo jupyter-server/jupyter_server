80d225e5376bace79fed8808646e724eb28ef544
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-05-03 14:31:13 +0000

    Publish 2.0.0a0
    SHA256 hashes:
    
    jupyter_server-2.0.0a0-py3-none-any.whl: 7be4a24d79bace14bdf83208e32b9403a38890f747ff889c107a19a5e702d6f9
    
    jupyter_server-2.0.0a0.tar.gz: 6704e2791a4a508d8e78d323ed7a5ff94a9f869fb06edd7d243b1830aea1c255

--- jupyter_server/_version.py
+++ jupyter_server/_version.py
@@ -2,5 +2,5 @@
 store the current version info of the server.
 
 """
-version_info = (1, 16, 1, ".dev", "0")
+version_info = (2, 0, 0, "a", "0")
 __version__ = ".".join(map(str, version_info[:3])) + "".join(version_info[3:])
