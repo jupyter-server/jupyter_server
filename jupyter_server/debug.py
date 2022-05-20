77bd3799e160749eae51f402c77a541616ba74ac
3coins <pyjain@gmail.com>
2022-05-19 21:59:04 -0700

    Added a debug file, vscode config  to aid debugging in IDE

---
+++ jupyter_server/debug.py
@@ -0,0 +1,7 @@
+"""Launches Juptyer Server. Use this file for debugging in an IDE."""
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+from serverapp import main
+
+if __name__ == "__main__":
+    main()
