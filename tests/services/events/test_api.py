c16ee965025937b1c5db88f0fb86f72138ed642a
3coins <pyjain@gmail.com>
2022-05-24 21:43:38 -0700

    Added e2e test for events

--- tests/services/events/test_api.py
+++ tests/services/events/test_api.py
@@ -1,27 +1,41 @@
+import io
 import json
 import logging
 import pathlib
 
 import pytest
 import tornado
+from jupyter_telemetry.eventlog import _skip_message
+from pythonjsonlogger import jsonlogger
 
 from tests.utils import expected_http_error
 
 
 @pytest.fixture
-def event_bus(jp_serverapp):
+def eventbus_sink(jp_serverapp):
     event_bus = jp_serverapp.event_bus
     # Register the event schema defined in this directory.
     schema_file = pathlib.Path(__file__).parent / "mock_event.yaml"
     event_bus.register_schema_file(schema_file)
-    #
     event_bus.allowed_schemas = ["event.mock.jupyter.org/message"]
-    event_bus.handlers = [logging.NullHandler()]
+
+    sink = io.StringIO()
+    formatter = jsonlogger.JsonFormatter(json_serializer=_skip_message)
+    handler = logging.StreamHandler(sink)
+    handler.setFormatter(formatter)
+    event_bus.handlers = [handler]
+    event_bus.log.addHandler(handler)
+
+    return event_bus, sink
+
+
+@pytest.fixture
+def event_bus(eventbus_sink):
+    event_bus, sink = eventbus_sink
     return event_bus
 
 
 async def test_subscribe_websocket(jp_ws_fetch, event_bus):
-    # Open a websocket connection.
     ws = await jp_ws_fetch("/api/events/subscribe")
 
     event_bus.record_event(
@@ -31,7 +45,6 @@ async def test_subscribe_websocket(jp_ws_fetch, event_bus):
     )
     message = await ws.read_message()
     event_data = json.loads(message)
-    # Close websocket
     ws.close()
 
     assert event_data.get("event_message") == "Hello, world!"
@@ -73,9 +86,17 @@ payload_4 = """\
 """
 
 
-async def test_post_event(jp_fetch, event_bus):
+async def test_post_event(jp_fetch, eventbus_sink):
+    event_bus, sink = eventbus_sink
+
     r = await jp_fetch("api", "events", method="POST", body=payload_1)
     assert r.code == 204
+
+    output = sink.getvalue()
+    assert output
+    input = json.loads(payload_1)
+    data = json.loads(output)
+    assert input["event"]["event_message"] == data["event_message"]
 
 
 @pytest.mark.parametrize("payload", [payload_2, payload_3, payload_4])
