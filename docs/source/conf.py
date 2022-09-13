0fc4fcc16cffec7119b5c7ba656c2cbf9f339eb1
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-09-13 18:56:04 +0000

    Publish 2.0.0rc0
    SHA256 hashes:
    
    jupyter_server-2.0.0rc0-py3-none-any.whl: 6962af692e6a4e9028a335b1ad7d684bc0bdb80a70278b5fe07320668af9448e
    
    jupyter_server-2.0.0rc0.tar.gz: 707bac1c29d795c7d84774e24545b5a9d1c9a6033313c5b1c72c327cf6127a89

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -108,7 +108,7 @@ github_project_url = "https://github.com/jupyter/jupyter_server"
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
-__version__ = "2.1.0.dev0"
+__version__ = "2.0.0rc0"
 # The short X.Y version.
 version_parsed = parse_version(__version__)
 version = f"{version_parsed.major}.{version_parsed.minor}"
