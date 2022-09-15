895a3804e400ec867bac4c3d7c462eb4087fdca0
Steven Silvester <steven.silvester@ieee.org>
2022-09-15 11:16:55 -0500

    Switch to using hatchling version command (#984)
    * switch to using hatchling version
    
    * revert changes to package-lock

--- jupyter_server/_version.py
+++ jupyter_server/_version.py
@@ -2,5 +2,17 @@
 store the current version info of the server.
 
 """
-version_info = (2, 1, 0, ".dev", "0")
-__version__ = ".".join(map(str, version_info[:3])) + "".join(version_info[3:])
+import re
+from typing import List
+
+# Version string must appear intact for tbump versioning
+__version__ = "2.1.0.dev0"
+
+# Build up version_info tuple for backwards compatibility
+pattern = r"(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)"
+match = re.match(pattern, __version__)
+assert match is not None
+parts: List[object] = [int(match[part]) for part in ["major", "minor", "patch"]]
+if match["rest"]:
+    parts.append(match["rest"])
+version_info = tuple(parts)
