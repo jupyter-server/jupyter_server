5c219219ec3269c7586085f0a9b599815c64acf5
Steven Silvester <steven.silvester@ieee.org>
2022-12-06 06:40:16 -0600

    compromise

--- jupyter_server/extension/application.py
+++ jupyter_server/extension/application.py
@@ -16,6 +16,7 @@ from jupyter_server.utils import is_namespace_package, url_path_join
 
 from .handler import ExtensionHandlerMixin
 
+
 # -----------------------------------------------------------------------------
 # Util functions and classes.
 # -----------------------------------------------------------------------------
