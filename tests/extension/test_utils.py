de8cb787a4d7d97fe39ae7da53edf362efda1fc4
Félix-Antoine Fortin <felix.antoine.fortin@gmail.com>
2023-02-03 12:48:00 -0500

    Backport #1193 to 1.x

--- tests/extension/test_utils.py
+++ tests/extension/test_utils.py
@@ -1,6 +1,11 @@
 import pytest
 
-from jupyter_server.extension.utils import validate_extension
+from jupyter_server.extension.utils import (
+    ExtensionLoadingError,
+    get_loader,
+    validate_extension,
+)
+from tests.extension.mockextensions import mockext_deprecated, mockext_sys
 
 # Use ServerApps environment because it monkeypatches
 # jupyter_core.paths and provides a config directory
@@ -17,3 +22,10 @@ def test_validate_extension():
     assert validate_extension("tests.extension.mockextensions.mockext_user")
     # enabled at Python
     assert validate_extension("tests.extension.mockextensions.mockext_py")
+
+def test_get_loader():
+    assert get_loader(mockext_sys) == mockext_sys._load_jupyter_server_extension
+    with pytest.deprecated_call():
+        assert get_loader(mockext_deprecated) == mockext_deprecated.load_jupyter_server_extension
+    with pytest.raises(ExtensionLoadingError):
+        get_loader(object())
