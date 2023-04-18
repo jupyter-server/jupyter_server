1c8798fc65dde6bb557a50acd1138d0f17b2d1c4
Divyansh Choudhary <divyanshchoudhary99@gmail.com>
2023-04-18 23:32:16 +0530

    Refactor methods

--- jupyter_server/auth/identity.py
+++ jupyter_server/auth/identity.py
@@ -82,37 +82,38 @@ class User:
             self.display_name = self.name
 
     @staticmethod
-    def user_to_cookie(user: User) -> str:
+    def to_string(self) -> str:
         """Serialize a user to a string for storage in a cookie
-
-        If overriding in a subclass, make sure to define user_from_cookie as well.
-
+        If overriding in a subclass, make sure to define from_string as well.
         Default is just the user's username.
         """
         # default: username is enough
         cookie = json.dumps(
             {
-                "username": user.username,
-                "name": user.name,
-                "display_name": user.display_name,
-                "initials": user.initials,
-                "color": user.color,
+                "username": self.username,
+                "name": self.name,
+                "display_name": self.display_name,
+                "initials": self.initials,
+                "color": self.color,
             }
         )
         return cookie
 
-    @staticmethod
-    def user_from_cookie(cookie_value: str) -> User | None:
+    @classmethod
+    def from_string(cls, serialized_user: str) -> User:
         """Inverse of user_to_cookie"""
-        user = json.loads(cookie_value)
-        return User(
-            user["username"],
-            user["name"],
-            user["display_name"],
-            user["initials"],
-            None,
-            user["color"],
-        )
+        user = json.loads(serialized_user)
+        try:
+            return User(
+                user["username"],
+                user["name"],
+                user["display_name"],
+                user["initials"],
+                None,
+                user["color"],
+            )
+        except:
+            raise
 
 
 def _backward_compat_user(got_user: Any) -> User:
@@ -349,7 +350,7 @@ class IdentityProvider(LoggingConfigurable):
             cookie_options.setdefault("secure", True)
         cookie_options.setdefault("path", handler.base_url)
         cookie_name = self.get_cookie_name(handler)
-        handler.set_secure_cookie(cookie_name, User.user_to_cookie(user), **cookie_options)
+        handler.set_secure_cookie(cookie_name, user.to_string(), **cookie_options)
 
     def _force_clear_cookie(
         self, handler: JupyterHandler, name: str, path: str = "/", domain: str | None = None
@@ -406,7 +407,7 @@ class IdentityProvider(LoggingConfigurable):
         user_cookie = _user_cookie.decode()
         # TODO: try/catch in case of change in config?
         try:
-            return User.user_from_cookie(user_cookie)
+            return User.from_string(user_cookie)
         except Exception as e:
             # log bad cookie itself, only at debug-level
             self.log.debug(f"Error unpacking user from cookie: cookie={user_cookie}", exc_info=True)
