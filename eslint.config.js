faf4692d9891d73a066f0e8b8daa662578c1714f
Steven Silvester <steve.silvester@mongodb.com>
2024-04-06 10:53:09 -0500

    Use eslint config

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        "parserOptions": {
+            "ecmaVersion": 6,
+            "sourceType": "module"
+        },
+        "rules": {
+            "semi": 1,
+            "no-cond-assign": 2,
+            "no-debugger": 2,
+            "comma-dangle": 0,
+            "no-unreachable": 2
+        },
+        "ignores":  [
+            "*.min.js",
+            "*components*",
+            "*node_modules*",
+            "*built*",
+            "*build*"
+        ]
+    }
+];
