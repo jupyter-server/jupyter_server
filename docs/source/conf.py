681bb3d1e82b8ab5b12ab027b4679ad48d566813
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2024-04-11 15:46:22 +0100

    Add html_context to enable links

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -162,6 +162,14 @@ html_theme_options = {
     "use_edit_page_button": True,
 }
 
+# Output for github to be used in links
+html_context = {
+    "github_user": "jupyter-server",  # Username
+    "github_repo": "jupyter_server",  # Repo name
+    "github_version": "main",  # Version
+    "doc_path": "docs/source/",  # Path in the checkout to the docs root
+}
+
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
 
