c870f7e8b6d2508c227d7fe181485ac79e42ce1a
Omar Jarjur <ojarjur@google.com>
2024-06-06 00:36:44 +0000

    Remove sleep that caused test failures on some jobs

--- tests/services/kernels/test_execution_state.py
+++ tests/services/kernels/test_execution_state.py
@@ -12,14 +12,11 @@ import pytest
 from tornado.httpclient import HTTPClientError
 from traitlets.config import Config
 
-POLL_INTERVAL = 1
-
 
 async def test_execution_state(jp_fetch, jp_ws_fetch):
     r = await jp_fetch("api", "kernels", method="POST", allow_nonstandard_methods=True)
     kernel = json.loads(r.body.decode())
     kid = kernel["id"]
-    await poll_for_execution_state(kid, "idle", jp_fetch)
 
     # Open a websocket connection.
     ws = await jp_ws_fetch("api", "kernels", kid, "channels")
@@ -102,14 +99,6 @@ async def get_execution_state(kid, jp_fetch):
     r = await jp_fetch("api", "kernels", kid, method="GET")
     model = json.loads(r.body.decode())
     return model["execution_state"]
-
-
-async def poll_for_execution_state(kid, target_state, jp_fetch):
-    while True:
-        es = await get_execution_state(kid, jp_fetch)
-        if es == target_state:
-            return
-        time.sleep(POLL_INTERVAL)
 
 
 async def poll_for_parent_message_status(kid, parent_message_id, target_status, ws):
