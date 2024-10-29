44c24817be628ee9eb12e499bd08b41f5fde5901
YuviPanda <yuvipanda@gmail.com>
2024-10-28 20:56:38 -0700

    Fix circular import when nbclassic shim is present?

--- jupyter_server/prometheus/metrics.py
+++ jupyter_server/prometheus/metrics.py
@@ -4,26 +4,23 @@ Prometheus metrics exported by Jupyter Server
 Read https://prometheus.io/docs/practices/naming/ for naming
 conventions for metrics & labels.
 """
-# Do this to see if we end up in an import loop?
-from notebook.prometheus.metrics import (
-    HTTP_REQUEST_DURATION_SECONDS,
-    KERNEL_CURRENTLY_RUNNING_TOTAL,
-    TERMINAL_CURRENTLY_RUNNING_TOTAL,
-)
+from prometheus_client import Info, Gauge, Histogram
 
 try:
-    # Jupyter Notebook also defines these metrics.  Re-defining them results in a ValueError.
-    # Try to de-duplicate by using the ones in Notebook if available.
+    from notebook._version import version_info as notebook_version_info
+except ImportError:
+    notebook_version_info = None
+
+if not notebook_version_info >= (7,):
+    # Jupyter Notebook v6 also defined these metrics.  Re-defining them results in a ValueError,
+    # so we simply re-export them if we are co-existing with the notebook v6 package.
     # See https://github.com/jupyter/jupyter_server/issues/209
     from notebook.prometheus.metrics import (
         HTTP_REQUEST_DURATION_SECONDS,
         KERNEL_CURRENTLY_RUNNING_TOTAL,
         TERMINAL_CURRENTLY_RUNNING_TOTAL,
     )
-
-except ImportError:
-    from prometheus_client import Gauge, Histogram
-
+else:
     HTTP_REQUEST_DURATION_SECONDS = Histogram(
         "http_request_duration_seconds",
         "duration in seconds for all HTTP requests",
@@ -41,9 +38,7 @@ except ImportError:
         ["type"],
     )
 
-
-from prometheus_client import Info
-
+# New prometheus metrics that do not exist in notebook v6 go here
 SERVER_INFO = Info("jupyter_server", "Jupyter Server Version information")
 
 __all__ = [
