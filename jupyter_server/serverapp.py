47b53c03cd9ad3139b193ca9b9f741d6f27a2c3b
Simon Li <orpheus+devel@gmail.com>
2025-01-26 13:32:31 +0000

    display_url: Don't duplicate public_url and local_url if they are the same

--- jupyter_server/serverapp.py
+++ jupyter_server/serverapp.py
@@ -2385,7 +2385,9 @@ class ServerApp(JupyterApp):
         """Human readable string with URLs for interacting
         with the running Jupyter Server
         """
-        url = self.public_url + "\n    " + self.local_url
+        url = self.public_url
+        if self.public_url != self.local_url:
+            url = f"{url}\n    {self.local_url}"
         return url
 
     @property
