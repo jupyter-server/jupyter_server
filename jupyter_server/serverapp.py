edb2f1b6dec9c628056c20548246083a289a0751
Simon Li <orpheus+devel@gmail.com>
2025-02-06 16:19:45 +0000

    If ServerApp.ip is ipv6 use [::1] as local_url (#1495)

--- jupyter_server/serverapp.py
+++ jupyter_server/serverapp.py
@@ -2377,7 +2377,8 @@ class ServerApp(JupyterApp):
         parts = self._get_urlparts(include_token=True)
         # Update with custom pieces.
         if not self.sock:
-            parts = parts._replace(netloc=f"127.0.0.1:{self.port}")
+            localhost = "[::1]" if ":" in self.ip else "127.0.0.1"
+            parts = parts._replace(netloc=f"{localhost}:{self.port}")
         return parts.geturl()
 
     @property
